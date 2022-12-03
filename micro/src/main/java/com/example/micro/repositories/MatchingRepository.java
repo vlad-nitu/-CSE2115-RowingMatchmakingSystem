66b4ce7fd3a7aa1279ee123e1f0979eb846be6ac
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-12-03 21:24:31 +0100

    Added @Repository annot.

--- micro/src/main/java/com/example/micro/repositories/MatchingRepository.java
+++ micro/src/main/java/com/example/micro/repositories/MatchingRepository.java
@@ -2,7 +2,9 @@ package com.example.micro.repositories;
 
 import com.example.micro.domain.Matching;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MatchingRepository extends JpaRepository<Matching, String> {
 
 }
