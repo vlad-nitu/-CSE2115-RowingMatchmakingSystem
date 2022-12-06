fdad11960829ffc9ee7311106b0e30f9df30c589
Razvan Loghin <R.Loghin@student.tudelft.nl>
2022-12-06 17:23:16 +0100

    Created the base of the activtiy microservice and imported the activity dataBase.

---
+++ activity-microservice/src/main/java/com/example/activitymicroservice/ActivityMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.activitymicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityMicroserviceApplication.class, args);
+    }
+
+}
