7eb52207bf6f43b46622a01fcf02fbd1f35e1505
Radu Nicolae <R.Nicolae@student.tudelft.nl>
2022-12-09 16:53:32 +0100

    Checkstyle fix

--- authentication-microservice/src/main/java/nl/tudelft/sem/template/authentication/domain/user/AppUser.java
+++ authentication-microservice/src/main/java/nl/tudelft/sem/template/authentication/domain/user/AppUser.java
@@ -2,8 +2,13 @@ package nl.tudelft.sem.template.authentication.domain.user;
 
 import java.util.Objects;
 import javax.annotation.processing.Generated;
-import javax.persistence.*;
-
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import lombok.NoArgsConstructor;
 import net.bytebuddy.dynamic.loading.InjectionClassLoader;
 import nl.tudelft.sem.template.authentication.domain.HasEvents;
