4197ad8058cb8d6f77792b52ac3083c3f8c920a3
Lotte Kremer <l.a.a.kremer@student.tudelft.nl>
2022-12-11 20:10:12 +0100

    Add a bit of structure

---
+++ user/src/main/java/nl/tudelft/cse/sem/template/user/domain/User.java
@@ -0,0 +1,11 @@
+package nl.tudelft.cse.sem.template.user.domain;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.NoArgsConstructor;
+
+//@Entity
+//@Table(name = "Users")
+//@NoArgsConstructor
+//public class User {
+//}
---
+++ user/src/main/java/nl/tudelft/cse/sem/template/user/domain/UserRepository.java
@@ -0,0 +1,8 @@
+package nl.tudelft.cse.sem.template.user.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+//@Repository
+//public interface UserRepository extends JpaRepository<User, String> {
+//}
