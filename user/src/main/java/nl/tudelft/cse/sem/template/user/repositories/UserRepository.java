76f9a6b189dbab5e5d05bea7dfa58dab6338ede6
Lotte Kremer <l.a.a.kremer@student.tudelft.nl>
2022-12-14 11:21:21 +0100

    Set up User and UserRepository

--- user/src/main/java/nl/tudelft/cse/sem/template/user/repositories/UserRepository.java
+++ user/src/main/java/nl/tudelft/cse/sem/template/user/repositories/UserRepository.java
@@ -1,12 +1,8 @@
 package nl.tudelft.cse.sem.template.user.repositories;
 
 import nl.tudelft.cse.sem.template.user.domain.User;
-import nl.tudelft.cse.sem.template.user.utils.TimeSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
