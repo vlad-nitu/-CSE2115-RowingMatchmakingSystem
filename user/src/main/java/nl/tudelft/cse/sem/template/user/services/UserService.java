b7b7eab2529c3a1f38908cc0be8dff24e69fb390
Dawid Karol Plona <D.K.Plona@student.tudelft.nl>
2022-12-14 12:59:44 +0100

    Create basic methods for querying the User database

--- user/src/main/java/nl/tudelft/cse/sem/template/user/services/UserService.java
+++ user/src/main/java/nl/tudelft/cse/sem/template/user/services/UserService.java
@@ -1,4 +1,91 @@
 package nl.tudelft.cse.sem.template.user.services;
 
+
+import lombok.AllArgsConstructor;
+import nl.tudelft.cse.sem.template.user.domain.User;
+import nl.tudelft.cse.sem.template.user.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@AllArgsConstructor
+@Service
 public class UserService {
+
+    private final transient UserRepository userRepository;
+
+    /**
+     * Finds all the users stored in the database
+     * @return List<User> containing all the users persisted in the repository
+     */
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    /**
+     * Saves a new user in the database
+     * @param user
+     * @return User object containing the saved user
+     */
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    /**
+     * Finds competitiveness corresponding to the given userId
+     * @param userId - the ID of the user
+     * @return boolean defining whether the user is competetive
+     */
+    public boolean findCompetitivenessByUserId(String userId){
+        Optional<User> user = userRepository.findById(userId);
+        return user.get().isCompetitive();
+    }
+
+    /**
+     * Finds the gender corresponding to the given userId
+     * @param userId - the ID of the user
+     * @return character defining the user's gender
+     */
+    public Character findGenderById(String userId){
+        Optional<User> user = userRepository.findById(userId);
+        return user.get().getGender();
+    }
+
+
+    /**
+     * Finds the certificate possessed by the user with the given ID
+     * @param userId - the ID of the user
+     * @return String containing the user's certificate
+     */
+    public String findCertificateById(String userId){
+        Optional<User> user = userRepository.findById(userId);
+        return user.get().getCertificate();
+    }
+
+
+    /**
+     * Finds the organisation corresponding to the given userId
+     * @param userId - the ID of the user
+     * @return String containing the organisation the user is a part of
+     */
+    public String findOrganisationById(String userId){
+        Optional<User> user = userRepository.findById(userId);
+        return user.get().getOrganisation();
+    }
+
+
+    /**
+     * Finds the positions the user with the given userId is capable of filling
+     * @param userId - the ID of the user
+     * @return Set<String> containing the positions the user is able to fulfill
+     */
+    public Set<String> findPositionsById(String userId){
+        Optional<User> user = userRepository.findById(userId);
+        return user.get().getPositions();
+    }
+
+
+
 }
