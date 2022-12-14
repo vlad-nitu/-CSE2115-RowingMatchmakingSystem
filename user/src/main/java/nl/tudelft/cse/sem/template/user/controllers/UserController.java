1edc7c6cde340f9c5ba15eef01049b64249a92af
Dawid Karol Plona <D.K.Plona@student.tudelft.nl>
2022-12-14 13:51:09 +0100

    Create a util class for basic input validation

--- user/src/main/java/nl/tudelft/cse/sem/template/user/controllers/UserController.java
+++ user/src/main/java/nl/tudelft/cse/sem/template/user/controllers/UserController.java
@@ -1,7 +1,8 @@
 package nl.tudelft.cse.sem.template.user.controllers;
 
+import nl.tudelft.cse.sem.template.user.services.UserService;
+import nl.tudelft.cse.sem.template.user.utils.InputValidation;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import nl.tudelft.cse.sem.template.user.domain.User;
-
-import javax.ws.rs.Path;
 
 @RestController
 public class UserController {
@@ -20,24 +19,24 @@ public class UserController {
     /**
      * All argument constructor, injects the main service component and all (4 now 0) publishers into the controller.
      *
-     * @param UserService - user service implementation
+     * @param userService - user service implementation
      */
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @PostMapping(/createUser)
+    @PostMapping("/createUser")
     public ResponseEntity<User> createUser(@RequestBody User user){
         return ResponseEntity.ok(userService.save(user));
     }
 
-    @GetMapping(/sendCompetitiveness/{userId})
-    public ResponseEntity<Boolean> sendCompetitiveness(@PathVariable userId){
-        return ResponseEntity.ok(userService.)
+    @GetMapping("/sendCompetitiveness/{userId}")
+    public ResponseEntity<Boolean> sendCompetitiveness(@PathVariable String userId){
+        //return ResponseEntity.ok(userService.)
     }
 
-    @GetMapping(/sendGender/{userId})
-    public ResponseEntity<Character> sendGender(@PathVariable userId) {
-        return ResponseEntity.ok(userService.);
+    @GetMapping("/sendGender/{userId}")
+    public ResponseEntity<Character> sendGender(@PathVariable String userId) {
+        //return ResponseEntity.ok(userService.);
     }
 }
---
+++ user/src/main/java/nl/tudelft/cse/sem/template/user/utils/InputValidation.java
@@ -0,0 +1,29 @@
+package nl.tudelft.cse.sem.template.user.utils;
+
+import lombok.AllArgsConstructor;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+@AllArgsConstructor
+public class InputValidation {
+
+    private static final Pattern patternNoSpecialCharacters = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+
+    /**
+     * Validates the userId by checking if it contains special characters, is null, empty and withing the desired length.
+     *
+     * @param userId - the ID of the user to be validated
+     * @return boolean defining whether the input passed the validation
+     */
+    public static boolean userIdValidation(String userId) {
+
+        Matcher matcher = patternNoSpecialCharacters.matcher(userId);
+
+        return userId != null && !userId.trim().isEmpty() && userId.length() > 3 && userId.length() < 20
+                && !matcher.find();
+
+    }
+
+
+}
