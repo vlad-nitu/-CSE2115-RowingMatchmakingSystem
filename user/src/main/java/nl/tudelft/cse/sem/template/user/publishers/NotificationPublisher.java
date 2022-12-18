87f4e59dc8de4abe9d8da5bf9e4aca7e93e965df
Dawid Karol Plona <D.K.Plona@student.tudelft.nl>
2022-12-18 06:55:43 +0100

    Adapt NotificationPublisher.java getNotifications method to return a list of notifications

--- user/src/main/java/nl/tudelft/cse/sem/template/user/publishers/NotificationPublisher.java
+++ user/src/main/java/nl/tudelft/cse/sem/template/user/publishers/NotificationPublisher.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Generated
@@ -23,22 +25,21 @@ public class NotificationPublisher {
         this.userUtils = new UserUtils("http://localhost:8086/");
     }
 
-    // do we need a list of notifications here?
     /**
      * Requests all the notifications that are collected for the user and have not been seen yet.
      *
      * @param userId the id of the user requesting the notifications
-     * @return the received notification, if any are present
+     * @return a list containing the received notifications, if any are present
      */
-    public BaseNotification getNotifications(String userId) {
+    public List<BaseNotification> getNotifications(String userId) {
         try {
             @Cleanup
             Response res = userUtils.getRequest("/getNotifications/" + userId);
-            BaseNotification notification = res.readEntity(new GenericType<>() {});
-            return notification;
+            ArrayList<BaseNotification> notifications = res.readEntity(new GenericType<>() {});
+            return notifications;
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            return new BaseNotification();
+            return new ArrayList<>();
         }
     }
 
