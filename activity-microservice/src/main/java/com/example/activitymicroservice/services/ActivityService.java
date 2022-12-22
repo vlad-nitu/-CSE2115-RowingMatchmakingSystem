fa48e137edd736990793b87fcd3450999e597773
RazvanLoghin1 <89660559+RazvanLoghin1@users.noreply.github.com>
2022-12-22 19:07:25 +0200

    PMD violation fix.

--- activity-microservice/src/main/java/com/example/activitymicroservice/services/ActivityService.java
+++ activity-microservice/src/main/java/com/example/activitymicroservice/services/ActivityService.java
@@ -14,6 +14,7 @@ import java.util.*;
 
 @AllArgsConstructor
 @Service
+@SuppressWarnings("PMD.DataflowAnomalyAnalysis")
 public class ActivityService {
 
     private static LocalDateTime checkTime;
