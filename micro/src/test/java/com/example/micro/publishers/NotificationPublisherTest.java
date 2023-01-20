c1061119fa96e642d87adc4f81c6efcea314e524
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2023-01-20 15:26:08 +0100

    Improve notifyUser for mutation score 100%
    Took 12 minutes

--- micro/src/test/java/com/example/micro/publishers/NotificationPublisherTest.java
+++ micro/src/test/java/com/example/micro/publishers/NotificationPublisherTest.java
@@ -1,5 +1,6 @@
 package com.example.micro.publishers;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -7,14 +8,23 @@ import static org.mockito.Mockito.when;
 import com.example.micro.utils.BaseNotification;
 import com.example.micro.utils.MatchingUtils;
 import javax.ws.rs.core.Response;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 @ExtendWith(MockitoExtension.class)
 public class NotificationPublisherTest {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
     @Mock
     private MatchingUtils matchingUtils;
 
@@ -23,6 +33,12 @@ public class NotificationPublisherTest {
     @BeforeEach
     void setUp() {
         notificationPublisher = new NotificationPublisher(matchingUtils);
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        System.setOut(standardOut);
     }
 
     @Test
@@ -39,5 +55,7 @@ public class NotificationPublisherTest {
         BaseNotification notification = new BaseNotification("Nicu", 1L, "iron", "notifyOwner");
         when(matchingUtils.postRequest("/notifyUser", notification)).thenThrow(new Exception("lol"));
         notificationPublisher.notifyUser(notification);
+
+        assertThat(outputStreamCaptor.toString().trim()).isEqualTo("lol");
     }
 }
